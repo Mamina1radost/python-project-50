d02646cc43a36a93c11f847eca6807543a9ddc9e
Slavyan <GlockOglock@yandex.ru>
2024-10-13 01:33:51 +0300

    sdvdg

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,33 +1,43 @@
 from typing import Union
 
 
+def value_to_plain(value: bool | None | int | str | dict):
+    
+    if '{' in str(value):
+        return "[complex value]"
+
+    if type(value) is int:
+        return value
+    if type(value) is bool:
+        result = str(value)
+        result = result.lower()
+        return result
+    if value == None:
+        return 'null'
+    if type(value) is str:
+        return f"'{value}'"
+
+
+
+
 def plain(
     result_dict: dict[str, Union[dict, str, int, bool, list]], base_key=""
 ) -> str:
     result = ""
     for key, value in result_dict.items():
         prefix = key if base_key == "" else f"{base_key}.{key}"
+        c = ' '
         if value[0] == "added":
             result += f"Property '{prefix}' was added with value: {value_to_plain(value[1])}\n"
         if value[0] == "deleted":
             result += f"Property '{prefix}' was removed\n"
+        if value[0] == "changed":
+            #result += f"Property '{prefix}' was updated. From {value_to_plain(value[1])} to {value_to_plain(value[2])}\n"
+            a = value_to_plain(value[1])
+            b = value_to_plain(value[2])
+            result += f"Property '{prefix}' was updated. From {a} to {b}\n"
+            result+= f''
         if value[0] == "recursiv":
             result += plain(value[1], prefix)
-        if value[0] == "changed":
-            result += f"Property '{prefix}' was updated. From {value_to_plain(value[1])} to {value_to_plain(value[2])}\n"
+
     return result
-
-
-def value_to_plain(value: bool | None | int | str | dict):
-    if value == 0:
-        return 'None'
-    if type(value) is bool:
-        result = str(value)
-        result = result.lower()
-        return result
-    if value is None:
-        return 'null'
-    if type(value) is str:
-        return f"'{value}'"
-    if type(value) is dict:
-        return "[complex value]"
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -7,11 +7,18 @@ from yaml.loader import SafeLoader
 
 def test_choise_format():
     format = "plain"
-    first = json.load(open("tests/fixtures/file1.json"))
+    '''first = json.load(open("tests/fixtures/file1.json"))
     second = json.load(open("tests/fixtures/file2.json"))
     result = open("tests/fixtures/plain.txt").read()
 
+    assert choise_format(first, second, format) == result'''
+
+    first = json.load(open("tests/fixtures/file3.json"))
+    second = json.load(open("tests/fixtures/file4.json"))
+    result = open("tests/fixtures/plain_2.txt").read()
+
     assert choise_format(first, second, format) == result
+
 
     first = yaml.load(open("tests/fixtures/file1.yaml"), Loader=SafeLoader)
     second = yaml.load(open("tests/fixtures/file2.yaml"), Loader=SafeLoader)
@@ -40,3 +47,5 @@ def test_choise_format():
     second = yaml.load(open("tests/fixtures/file2.yaml"), Loader=SafeLoader)
 
     assert choise_format(first, second, format) == result
+
+
